b890b95d06c2bf085ac86afa003c4303696dbd85
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-05-14 01:10:42 +0530

    Initial Commit

---
+++ Light Weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Light Weather
+//
+//  Created by Nimrat Khalsa on 14/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Light Weather/Light_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_WeatherApp.swift
+//  Light Weather
+//
+//  Created by Nimrat Khalsa on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Light_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
